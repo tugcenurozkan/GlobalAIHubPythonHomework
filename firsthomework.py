a1fa23d67b3de41f44aa537681d045685aa55c01
tugcenurozkan <tugceozkan85@gmail.com>
2020-12-22 18:15:41 +0300

    first commit

---
+++ firsthomework.py
@@ -0,0 +1,24 @@
+#KULLANICIDAN ALINAN SAYININ ÇIKTISI VE TÜRÜNÜ VEREN PROGRAM
+mylist=[]
+num=0
+while num<5:
+     x= input("Enter a thing: ")
+     mylist.append(x)
+     num+=1
+
+print(mylist)
+number=0
+
+for i in mylist:
+
+ try:
+   number==int (i)
+   print (f'input:{i}','/','type is integer')
+         
+ except ValueError:  
+  print('input {}'.format(i),'/','type is string')
+  
+
+  
+
+     
