275a08f4d2a40c302be2a9525c4577699a7785c4
Tuğçe <66311849+tugcenurozkan@users.noreply.github.com>
2020-12-22 22:49:36 +0300

    Add files via upload

---
+++ secondhomework.py
@@ -0,0 +1,28 @@
+firstname=input("Please write your firstname:")
+lastname=input("Please write your lastname:")
+dateOfBirth=int(input("Please write your birth year:"))
+age=int(input("Please your age:"))
+
+userinfo=[]
+userinfo.append(firstname)
+userinfo.append(lastname)
+userinfo.append(dateOfBirth)
+userinfo.append(age)
+
+print("\n")
+
+print("User's information:")
+
+for i in userinfo:
+  print(i)
+
+print("\n")
+
+if age<18:
+    print("You can't go out  because it's too dangerous.")
+
+else:
+    print("You can go out to the street.")
+
+print("\n")
+
